8fcf668f1f70d3631d1e00a1528ea0ffbbb10cdb
fvucemilo <fvucemilo@tvz.hr>
2024-09-17 00:04:48 +0200

    Inital setup

---
+++ tests/utils/setupTestDb.ts
@@ -0,0 +1,21 @@
+import { beforeAll, beforeEach, afterAll } from '@jest/globals';
+import prisma from '../../src/client';
+
+const setupTestDB = () => {
+  beforeAll(async () => {
+    await prisma.$connect();
+  });
+
+  beforeEach(async () => {
+    await prisma.token.deleteMany();
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.token.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.$disconnect();
+  });
+};
+
+export default setupTestDB;
